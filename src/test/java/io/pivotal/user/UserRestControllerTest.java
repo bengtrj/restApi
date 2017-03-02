eac0834fd1550d9e1a5df8b4708816777373e104
Pivotal <pivotal@Henry.local>
2017-03-02 19:53:11 +0000

    Making the test class public

--- src/test/java/io/pivotal/user/UserRestControllerTest.java
+++ src/test/java/io/pivotal/user/UserRestControllerTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RestApiApplication.class)
 @WebAppConfiguration
-class UserRestControllerTest {
+public class UserRestControllerTest {
 
     private static final String APPLICATION_JSON = "application/json;charset=UTF-8";
 
